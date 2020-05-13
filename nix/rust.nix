6038160ab2400df05e3310b107378121817a593b
Cassie Meharry <cassie@prophetessof.tech>
2020-05-13 19:50:05 +0000

    Upgrade Rust nightly version

--- nix/rust.nix
+++ nix/rust.nix
@@ -6,6 +6,6 @@ let
   pkgs = import sources.nixpkgs {
     overlays = [ (import sources.nixpkgs-mozilla) ];
   };
-  channel = pkgs.rustChannelOfTargets "nightly" "2020-04-20" targets;
+  channel = pkgs.rustChannelOfTargets "nightly" "2020-05-10" targets;
 in
 channel
