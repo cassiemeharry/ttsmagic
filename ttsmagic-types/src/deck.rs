253dbe9f638dcddb7bb3364fdef28cb7d4a892d6
Cassie Meharry <cassie@prophetessof.tech>
2020-09-08 23:51:08 +0000

    Use colors for DeckColorIdentity Debug impl

--- ttsmagic-types/src/deck.rs
+++ ttsmagic-types/src/deck.rs
@@ -41,13 +41,59 @@ impl fmt::Display for DeckId {
     }
 }
 
-#[derive(Copy, Clone, Debug, Default, Eq, PartialEq, Hash, Deserialize, Serialize)]
+#[derive(Copy, Clone, Default, Eq, PartialEq, Hash, Deserialize, Serialize)]
 pub struct DeckColorIdentity {
     pub black: bool,
     pub blue: bool,
     pub green: bool,
     pub red: bool,
     pub white: bool,
+}
+
+impl fmt::Debug for DeckColorIdentity {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        const BLACK: &'static str = "\x1b[37;40mblack\x1b[0m";
+        const BLUE: &'static str = "\x1b[34mblue\x1b[0m";
+        const GREEN: &'static str = "\x1b[32mgreen\x1b[0m";
+        const RED: &'static str = "\x1b[31mred\x1b[0m";
+        const WHITE: &'static str = "\x1b[30;47mwhite\x1b[0m";
+        let colors: &'static [&str] =
+            match (self.black, self.blue, self.green, self.red, self.white) {
+                (false, false, false, false, false) => &["colorless"],
+                (false, false, false, false, true) => &[WHITE],
+                (false, false, false, true, false) => &[RED],
+                (false, false, false, true, true) => &[RED, WHITE],
+                (false, false, true, false, false) => &[GREEN],
+                (false, false, true, false, true) => &[GREEN, WHITE],
+                (false, false, true, true, false) => &[GREEN, RED],
+                (false, false, true, true, true) => &[GREEN, RED, WHITE],
+                (false, true, false, false, false) => &[BLUE],
+                (false, true, false, false, true) => &[BLUE, WHITE],
+                (false, true, false, true, false) => &[BLUE, RED],
+                (false, true, false, true, true) => &[BLUE, RED, WHITE],
+                (false, true, true, false, false) => &[BLUE, GREEN],
+                (false, true, true, false, true) => &[BLUE, GREEN, WHITE],
+                (false, true, true, true, false) => &[BLUE, GREEN, RED],
+                (false, true, true, true, true) => &[BLUE, GREEN, RED, WHITE],
+                (true, false, false, false, false) => &[BLACK],
+                (true, false, false, false, true) => &[BLACK, WHITE],
+                (true, false, false, true, false) => &[BLACK, RED],
+                (true, false, false, true, true) => &[BLACK, RED, WHITE],
+                (true, false, true, false, false) => &[BLACK, GREEN],
+                (true, false, true, false, true) => &[BLACK, GREEN, WHITE],
+                (true, false, true, true, false) => &[BLACK, GREEN, RED],
+                (true, false, true, true, true) => &[BLACK, GREEN, RED, WHITE],
+                (true, true, false, false, false) => &[BLACK, BLUE],
+                (true, true, false, false, true) => &[BLACK, BLUE, WHITE],
+                (true, true, false, true, false) => &[BLACK, BLUE, RED],
+                (true, true, false, true, true) => &[BLACK, BLUE, RED, WHITE],
+                (true, true, true, false, false) => &[BLACK, BLUE, GREEN],
+                (true, true, true, false, true) => &[BLACK, BLUE, GREEN, WHITE],
+                (true, true, true, true, false) => &[BLACK, BLUE, GREEN, RED],
+                (true, true, true, true, true) => &[BLACK, BLUE, GREEN, RED, WHITE],
+            };
+        write!(f, "DeckColorIdentity {{ {} }}", colors.join(", "))
+    }
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, Hash, Deserialize, Serialize)]
